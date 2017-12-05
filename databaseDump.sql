fe0c9e53d8197a2bc781528d26f064ed2d5c71da
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2017-12-05 23:26:34 +0100

    Add database dump

---
+++ databaseDump.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Dec 05, 2017 at 11:25 PM
+-- Server version: 5.6.33
+-- PHP Version: 7.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `af`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `individuals`
+--
+
+CREATE TABLE `individuals` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `company_id` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `individuals`
+--
+ALTER TABLE `individuals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `individuals`
+--
+ALTER TABLE `individuals`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
